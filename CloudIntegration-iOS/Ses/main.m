7b892321d1f6b68bd45333476a318220fa9c1026
Smartface <oytun.sengul@mobinex.biz>
2014-01-30 11:39:53 +0200

    Cloud Integration Sample Project

---
+++ CloudIntegration-iOS/Ses/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ses
+//
+//  Created by Smartface on 01.14.2014.
+//  Copyright (c) 2014 Smartface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CloudIntegration-iOS/SesTests/SesTests.m
@@ -0,0 +1,34 @@
+//
+//  SesTests.m
+//  SesTests
+//
+//  Created by Smartface on 01.14.2014.
+//  Copyright (c) 2014 Smartface. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SesTests : XCTestCase
+
+@end
+
+@implementation SesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
